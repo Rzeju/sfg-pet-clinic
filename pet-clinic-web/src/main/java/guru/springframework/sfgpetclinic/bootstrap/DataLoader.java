cd75852bc94c294a77438136a7de3c6e6de0fa43
pandrzejak <piotr.andrzejak@primaris.eu>
2020-11-15 07:32:50 +0100

    Manage setting of ID Property. Closes #24

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,21 +23,18 @@ public class DataLoader implements CommandLineRunner {
 
         Owner owner1 = new Owner();
 
-        owner1.setId(1L);
         owner1.setFirstName("John");
         owner1.setLastName("Smith");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Michael");
         owner2.setLastName("Ruth");
 
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
-        owner3.setId(3L);
         owner3.setFirstName("Kyle");
         owner3.setLastName("Broflowsky");
 
@@ -46,14 +43,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Amanda");
         vet1.setLastName("Stonebreaker");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2L);
         vet2.setFirstName("Julia");
         vet2.setLastName("Rogers");
 
